9f0f8969c68080a76de0dab30713b4fbef8e7d9c
sven-warnke <sven@ge-warnke.de>
2024-03-02 20:57:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+local_css("styles.css")
 
-st.number_input("Health", 0, 50, 50)
-st.number_input("Focus", 0, 30, 30)
+st.form:
+    st.number_input("Health", 0, 50, 50)
+    st.number_input("Focus", 0, 30, 30)
