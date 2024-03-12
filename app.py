b523a573b4b88cf5ae9286ad667a6b746be61f77
SvenWarnke <44029716+SvenWarnke@users.noreply.github.com>
2024-03-12 23:19:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import streamlit as st
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+import streamlit.components.v1 as components
 
-local_css("styles.css")
 
-st.form:
-    st.number_input("Health", 0, 50, 50)
-    st.number_input("Focus", 0, 30, 30)
+components.html(
+    """
+    <body>
+
+    <input aria-label="Health" aria-invalid="false" aria-required="false" autocomplete="on" inputmode="text" name="" placeholder="" type="number" min="0" max="50" step="1" data-testid="stNumberInput-Input" value="50" style="background-color: green; color: red; font-size: 1000%; height:400px; width:400px; align: 'center';">
+
+    <input aria-label="Health" aria-invalid="false" aria-required="false" autocomplete="on" inputmode="text" name="" placeholder="" type="number" min="0" max="30" step="1" data-testid="stNumberInput-Input" value="0" style="background-color: yellow; color: black; font-size: 1000%; height:400px; width:400px; align: 'center';">
+    </body>
+    """,
+    scrolling=True,
+    height=830,
+    width=430,
+)
